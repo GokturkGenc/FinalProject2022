b7a6c54a1ec0e244230f58452c70f0e4be9c88fe
Göktürk Genç <hgokturkgenc@gmail.com>
2022-10-10 15:51:55 +0300

    Code Refactoring, Added Results and Messages

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -27,10 +27,21 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success)
             {
-                Console.WriteLine(product.ProductName + "\t" + product.CategoryName);
+                foreach (var product in productManager.GetProductDetails().Data)
+                {
+                    Console.WriteLine(product.ProductName + "\t" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+
         }
     }
 }
